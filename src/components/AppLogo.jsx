4208500d1d5a8af0be141d367ff91346fc649382
Mohamed Magdy <mohamedmagdyelsayed7@gmail.com>
2026-09-11 17:46:25 +0300

    fix app logo image path

--- src/components/AppLogo.jsx
+++ src/components/AppLogo.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/images/Logo.webp";
+import logo from "../assets/images/logo.webp";
 
 export default function AppLogo() {
   return <img src={logo} alt="Chat App Logo" className="w-50" />;
